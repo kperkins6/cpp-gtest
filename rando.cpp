b3d140bdd804e6dc87bb8f9c4092988bf1ac7601
Kevin Perkins <kperkins6@mail.csuchico.edu>
2015-09-23 11:25:17 -0700

    Was editing the wrong repo...

--- rando.cpp
+++ rando.cpp
@@ -4,39 +4,46 @@
 
 /**
  * Worry if only one child is smiling or if all children are smiling
-**/
+ **/
 bool Rando::shouldWorry(bool childASmile, bool childBSmile, bool childCSmile)
 {
-	if( childASmile && childBSmile && childCSmile )
-		return true;
-	else if( childASmile ^ childBSmile ^ childCSmile )
-		return true;
-	else
-		return false;
+    if( childASmile && childBSmile && childCSmile )
+        return true;
+    else if( childASmile ^ childBSmile ^ childCSmile )
+        return true;
+    else
+        return false;
 }
 /**
  * Determines if first or second are evenly divisible by the other.
-**/
+ **/
 bool Rando::isDivisibleBy(int first, int second)
 {
-	return false;
-}
+    if ( first == 0 && second == 0)
+        throw -1; //This is the exception that we throw, exception -1
+    if ( !(first % second))
+        return true;
+    else if (!(second % first))
+        return true;
+    return false;
+}// 0,0  5,0 4,2 3,11  4,4 Do test cases for all of these
 
 /**
  * Given a number, num, says whether or not that number is prime
  * (that is, only evenly divisible by itself and 1. For the sake
  * of this function, do NOT consider 0 prime)
-**/
+ **/
 bool Rando::isPrime(int num)
 {
-	return true;
+    return true;
 }
 
 /**
  * Given two numbers, first and second, return the value of the one
  * that is closest to zero without actually being zero.
-**/
+ **/
 int Rando::nearestToZero(int a, int b)
 {
-	return 0;
+    return 0;
 }
+
--- randoTest.cpp
+++ randoTest.cpp
@@ -1,36 +1,58 @@
 /**
  * Unit Tests for Rando Test
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "rando.h"
 
-/*  
+/*
 	FUNCTIONS WE NEED TO TEST:
-
+ 
 	bool shouldWorry(bool,bool,bool);
 	bool isDivisbleBy(int,int);
 	bool isPrime(int);
 	int nearestToZero(int,int);
-*/
- 
+ */
+
 class RandoTest : public ::testing::Test
 {
-	protected:
-		RandoTest(){} //constructor runs before each test
-		virtual ~RandoTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+protected:
+    RandoTest(){} //constructor runs before each test
+    virtual ~RandoTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(RandoTest, allChildrenSmile)
 {
-	Rando rando;
-	ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+    Rando rando;
+    ASSERT_TRUE( rando.shouldWorry(true,true,true) );
 }
 
 TEST(RandoTest, nearestToZero)
+{
+    Rando rando;
+    ASSERT_EQ( rando.nearestToZero(0, 1));
+}
+
+
+TEST(RandoTest, isPrime)
+{
+    Rando rando;
+    ASSERT_TRUE( rando.isPrime(0));
+}
+
+
+TEST(RandoTest, isDivisbleBy)
+{
+    Rando rando;
+    try{ //A try block in C++, used to try things that throw exceptions
+        rando.isDivisbleBy(5, 0);
+    }catch(int ex) //Catches the exceptions
     {
-        Rando rando;
-        ASSERT_TRUE( rando.nearestToZero(0, 1));
+        if (ex ==-1) { //Looks for the exception. 0/0 should catch a try-catch. But 5, 0 would not, because 0/5 is divisible.
+            exceptionthrown = true;
+        }
     }
+    //Even divisible by even should always be true.
+}
